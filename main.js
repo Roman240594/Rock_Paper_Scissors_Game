8346cdfed1d58f7d92906a503d7a02b51207ccb9
Roman <linevychroman@gmail.com>
2023-03-02 18:53:10 +0200

    add function choose sign

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 const user = document.querySelectorAll('.name');
+const btn = document.querySelector('.sign__buttons');
+const userImage = document.querySelector('.choose__img-user');
+const computerImage = document.querySelector('.choose__img-computer');
+
+let userName;
+let userChoose;
+let signComputer;
+
+let signComputerArray = ['Rock', 'Paper', 'Scissors'];
+const signArray = [{
+        'name': 'Rock',
+        'image': 'image/rock.png'
+    },
+    {
+        'name': 'Paper',
+        'image': 'image/paper.png'
+    },
+    {
+        'name': 'Scissors',
+        'image': 'image/csissors.png'
+    }
+];
 
 let write = setTimeout(() => {
     userName = prompt('What is your name?');
@@ -16,3 +38,34 @@ let writeUserName = () => {
         }
     }
 };
+
+let choosedSign = () => {
+    userChoose = event.target.textContent;
+    for (let i = 0; i < signArray.length; i++) {
+        if (userChoose === signArray[i].name) {
+            addImage(userImage, signArray[i].image);
+            chooseComputer(signComputerArray);
+        }
+    };
+    countResult();
+}
+
+function chooseComputer(array) {
+    let result = Math.floor(Math.random() * array.length);
+    signComputer = array[result];
+    for (let i = 0; i < signArray.length; i++) {
+        if (array[result] === signArray[i].name) {
+            addImage(computerImage, signArray[i].image);
+        }
+    }
+};
+
+function addImage(player, link) {
+    player.src = link;
+};
+
+function showResult(player, point) {
+    player.innerText = point;
+}
+
+btn.addEventListener('click', choosedSign)
